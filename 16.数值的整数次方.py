d8003af4017e06f3dbd68235c3bbb22bb4de8ba0
飞飞想去踢球 <50862571+JingChufei@users.noreply.github.com>
2019-08-08 10:30:31 +0800

    Update 16.数值的整数次方.py

--- 16.数值的整数次方.py
+++ 16.数值的整数次方.py
@@ -34,3 +34,50 @@ class Solution:
     		res *= base
 
     	return res
+
+
+"""
+用递归优化 PowerWithUnsignedExponent():
+	例: 要求一个数的32次方, 我们先求16次方, 再取平方, 以此类推
+	公式:
+		a^n = a^(n / 2) * a^(n / 2) n为偶数
+		a^n = a^((n - 1) / 2) * a^((n - 1) / 2) * a n为奇数
+"""
+
+class Solution:
+
+	def __init__(self):
+		# 定义实例变量 来标识是否出错
+		self.g_InvalidInput = False
+
+    def Power(self, base, exponent):
+
+    	if base == 0 and exponent < 0:
+    		self.g_InvalidInput = True
+    		return 0
+
+    	if exponent >= 0:
+    		return self.PowerWithUnsignedExponent(base, exponent)
+
+    	return 1 / self.PowerWithUnsignedExponent(base, -exponent)
+
+
+    def PowerWithUnsignedExponent(self, base, exponent):
+
+    	# 特殊情况
+    	if exponent == 0:
+    		return 1
+
+    	# 递归终止条件
+    	if exponent == 1:
+    		return base
+
+    	res = self.PowerWithUnsignedExponent(base, exponent >> 1)
+    	res = res * res
+
+    	# 判断奇偶
+    	if exponent % 2 == 1:
+    		res = res * base
+
+    	return res
+    	
